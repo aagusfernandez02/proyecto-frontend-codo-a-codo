96a9f2f51c3b3e8aadbe12e84e26e1d387d3fa0c
Diego Gomareschi <50846060+die54@users.noreply.github.com>
2024-05-28 22:40:56 -0300

    validacion de form

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,16 +1,52 @@
-function validacion() 
-        {
-            valor = document.getElementById("firstName").value;
-            
-            if( valor == null || valor.length == 0)
-            {
-                
-                document.getElementById('alerta').innerHTML ="completar nombre";
-                elemento = document.getElementById('firstName');
-                elemento.focus();
-                
-                return false;
-            }
-            
-            return true;          
-        }     
+
+function validacion() {
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var birthdate = document.getElementById('birthdate').value;
+
+    var nombre = /^[a-zA-ZÀ-ÿ\s]{3,40}$/;
+    var apellido = /^[a-zA-ZÀ-ÿ\s]{2,40}$/;
+    var contraseña = /^[a-zA-ZÀ-ÿ\s-$-#].{8,12}$/;
+    var coreeo = /^[a-zA-Z0-9_.+-]+@(yahoo|gmail|hotmail|outlook)\.(com|.ar)$/;
+    var fecha = /^([0-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/;
+    
+    
+    if (!nombre.test(firstName)) {
+        document.getElementById('alerta').innerHTML = "Por favor, complete su nombre correctamente.";
+        document.getElementById('firstName').focus();
+        return false;
+    }
+
+    if (!apellido.test(lastName)) {
+        document.getElementById('alerta').innerHTML = "Por favor, complete su apellido correctamente.";
+        document.getElementById('lastName').focus();
+        return false;
+    }
+
+    if (!contraseña.test(password)) {
+        document.getElementById('alerta').innerHTML = "La contraseña debe tener entre 6 y 12 caracteres.";
+        document.getElementById('password').focus();
+        return false;
+    }
+
+   // if (!fecha.test(birthdate)) {
+     //   document.getElementById('alerta').innerHTML = "Por favor, ingrese una fecha de nacimiento válida.";
+       // document.getElementById('birthdate').focus();
+        //return false;
+    
+    if (!coreeo.test(email)) {
+        document.getElementById('alerta').innerHTML = "Por favor, ingrese un correo electrónico válido.";
+        document.getElementById('email').focus();
+        return false;
+    }
+
+    
+ 
+   
+
+    return true;
+
+    }   
+        
